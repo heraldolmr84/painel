6e3da4dd46da15e793d227ff1f6c99312a304318
heraldolmr84 <heraldoribeiro84@gmail.com>
2023-05-02 21:59:25 -0400

    02-05-2023 2159

--- src/main/java/com/painel/painel/IndexController.java
+++ src/main/java/com/painel/painel/IndexController.java
@@ -1,11 +1,21 @@
 package com.painel.painel;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.painel.painel.repositories.AmbienteRepository;
+import com.painel.painel.repositories.GuicheRepository;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private AmbienteRepository ar;
+
+    @Autowired
+    private GuicheRepository gr;
     
     @GetMapping("/")
     public ModelAndView index() {
@@ -26,6 +36,9 @@ public class IndexController {
     public ModelAndView chamarPainel() {
         
         ModelAndView mv = new ModelAndView("guicheChamada");
+
+        mv.addObject("list_ambiente", ar.findAll());
+        mv.addObject("list_guiche", gr.findAll());
 
         return mv;
     }
